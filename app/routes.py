2f4eb9a3b67214a454aa99a7efd5d66449b08380
plutos28 <gamevictor28@gmail.com>
2022-04-28 15:17:40 +0300

    move content to templates with base template for extending

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,23 @@
+from flask import render_template
+
 from app import flask_app
 
-@flask_app.route('/')
-@flask_app.route('/index')
+@flask_app.route("/")
+@flask_app.route("/index")
 def index():
-    return "Hello, fds"
+    user = {"username": "Victor"}
+    posts = [
+        {
+            "author": {"username": "Victor"},
+            "body": "Beautiful day in Juba!"
+        },
+        {
+            "author": {"username": "Bird"},
+            "body": "Attack on Titan ended horribly."
+        },
+        {
+            "author": {"username": "Johnny Boy"},
+            "body": "Damn I love to workout."
+        },
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
