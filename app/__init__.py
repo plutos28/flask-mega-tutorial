81cb97534eaf4501b8d9557415cfee89affea963
plutos28 <gamevictor28@gmail.com>
2022-05-02 21:56:20 +0300

    add dependencies for database and set it up

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 from config import Config
 
 flask_app = Flask(__name__)
 flask_app.config.from_object(Config)
+db = SQLAlchemy(flask_app)
+migrate = Migrate(flask_app, db)
 
 # import routes here instead because it needs to import the app variable
-from app import routes
+from app import routes, models
 
 
 
