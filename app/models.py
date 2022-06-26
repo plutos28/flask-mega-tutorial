6cc9f1b589af729d340c5adf0f889bfcff384ae3
plutos28 <gamevictor28@gmail.com>
2022-06-26 11:46:25 +0300

    add shell context so symbols are pre-imported

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic') # this is a virtual field, it's not in the actual db
 
     def __repr__(self):
         return f"<User {self.username}>"
@@ -18,3 +18,6 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f"<Post {self.id} - ({self.timestamp}) - {self.body[:30]}>"
--- microblog.py
+++ microblog.py
@@ -1,2 +1,9 @@
 from app import flask_app
+from app import db
+from app.models import User, Post
 
+@flask_app.shell_context_processor
+def make_shell_context():
+    # pre-import symbols to flask shell so that you don't have
+    # to import each time
+    return {'db': db, 'User': User, 'Post': Post}
