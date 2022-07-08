7ed3bec4c083dd97cdb80700ac6a78ce534f28db
plutos28 <gamevictor28@gmail.com>
2022-07-08 09:59:06 +0300

    display about me and last seen on profile and add edit page for profile

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,10 @@ from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
 from app import flask_app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
+
+from datetime import datetime
 
 
 @flask_app.route("/")
@@ -38,6 +40,32 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}, 
     ]
     return render_template('user.html', user=user, posts=posts)
+
+
+@flask_app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved')
+    elif request.method == 'GET':
+        # on get request, provide original values for editing
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+
+@flask_app.before_request
+def before_request():
+    # update the last seen value of the user before a request
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
 
 @flask_app.route('/register', methods=['GET', 'POST'])
 def register():
